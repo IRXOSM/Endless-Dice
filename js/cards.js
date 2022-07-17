a6ee6d4cff1f5681d565c73cbd61eff28d57f45f
IRXOSM <79060441+IRXOSM@users.noreply.github.com>
2022-07-17 23:14:38 +0200

    Update cards.js

--- js/cards.js
+++ js/cards.js
@@ -173,7 +173,7 @@ const CARDS = {
     curse1: [
         "Holy Multiplier",
         x=>`Multiply your number of side as well as your multiplier by <b class='green'>1.5</b>. <\n> <b class='red'>CAN ONLY BE PICKED UP ONCE.</b>`,
-        x=>!data[x].cards.includes("curse1") && math.random() < 1/10,
+        x=>!data[x].cards.includes("curse1") && Math.random() < 1/10,
         x=>{
             data[x].min_s *= 1.5
             data[x].max_s *= 1.5
